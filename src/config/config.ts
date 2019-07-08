cda90f86007af38d36b756ee1e50a1acb6ef5cda
krasserp <krasserp@gmail.com>
2019-07-08 20:43:55 +0100

    update config add feed

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,19 +1,28 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_reigion": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: process.env.UG_POSTGRESS_USERNAME,
+    password: process.env.UG_POSTGRESS_PASSWORD,
+    database: process.env.UG_POSTGRESS_DATABASE,
+    host: process.env.UG_POSTGRESS_HOST,
+    dialect: 'postgres',
+    aws: {
+      aws_reigion: process.env.UG_AWS_REGION,
+      aws_profile: process.env.UG_AWS_PROFILE,
+      aws_media_bucket: process.env.UG_AWS_MEDIA_BUCKET
+    },
+    url_expires: 60 * 5
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  prod: {
+    username: process.env.POSTGRESS_USERNAME,
+    password: process.env.POSTGRESS_PASSWORD,
+    database: process.env.POSTGRESS_DATABASE,
+    host: process.env.POSTGRESS_HOST,
+    dialect: 'postgres',
+    aws: {
+      aws_reigion: process.env.AWS_REGION,
+      aws_profile: process.env.AWS_PROFILE,
+      aws_media_bucket: process.env.AWS_MEDIA_BUCKET
+    },
+    url_expires: 60 * 5
   }
-}
+};
